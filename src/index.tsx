6a980d75ce1c2197f15d0e6447eb5660132ec647
Chetra Seng <sengchetra.love@icloud.com>
2025-07-30 16:56:38 +0700

    feat: Added last name to TelegramUser (#22)

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import React, { useRef, useEffect } from 'react'
 export interface TelegramUser {
   id: number
   first_name: string
+  last_name: string
   username: string
   photo_url: string
   auth_date: number
